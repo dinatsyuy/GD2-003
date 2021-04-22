ec6f27e3715abecf2c272230e25b910dad7da0e1
dinatsyuy <78235998+dinatsyuy@users.noreply.github.com>
2021-04-22 18:45:51 +0300

    a4

---
+++ main/script.js
@@ -0,0 +1,16 @@
+var element = $("#imageDIV"); // global variable
+var getCanvas; // global variable
+$('document').ready(function(){
+  html2canvas(element, {
+    onrendered: function (canvas) {
+      $("#previewImage").append(canvas);
+      getCanvas = canvas;
+    }
+  });
+});
+$("#download").on('click', function () {
+  var imgageData = getCanvas.toDataURL("image/png");
+  // Now browser starts downloading it instead of just showing it
+  var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+  $("#download").attr("download", "image.png").attr("href", newData);
+});
